b9372de5d9b740701189660691504315749b9914
Sepand Haghighi <sepand.haghighi@yahoo.com>
2015-04-13 01:50:43 +0430

    Add Header File

--- main.cpp
+++ main.cpp
@@ -17,10 +17,11 @@
 #include <fstream.h>
 #include <ctime.h>
 #include <fstream>
+#include <factor.h>
+int id=0;
+int number=0;
+char nam[256];
 
-
-int index=0;  // Main Index Init
-ofstream file;   // file as ofstream for saving factors
 
 void char_printer(int num , int kind){    // Print - or == for lines and split
  if (kind ==1) {
@@ -51,86 +52,6 @@ cout<<endl;
 
 
 
-class Factor{     // Factor Class   Init
-public :
-int num[256];       //Factor Numbers
-char name[256][256];   // Name In Factor
-int count[256] ;         // Count Of That Object
-double price[256];       //Price
-double total[256];       // Total Price
-
-
-
-void add_line ( int , char[]  , int , double);  // Add One Line To a Factor Object
-double total_price (Factor);      // Calc Toal Price
-double total_num(Factor);         // Calc Total Counts
-void print (Factor);              // Printing On Console
-void save (Factor , int);               // Saving In File
-};
-
- void Factor::add_line(int a, char b[256] , int d , double e){  //Add Line Method
- int i;
- int length;
- length = strlen(b);
-  for (i = 0; i < 31; i++) {
-	 name[index][i]=b[i];
- }
- if (length>=32) {
-	  name[index][31]=char(46);
-	  name[index][32]=char(46);
-	  name[index][33]=char(46);
- }
-
-
- num[index] = a;
- count[index] = d;
- price[index] = e;
- total[index]= e*d;
- index=index+1;
- }
- void Factor:: print(Factor a){  // Print Method
- int i;
- cout<<"#"<<"\t"<<"\t"<<"Name"<<"\t"<<"Quantitiy"<<"\t"<<"Price"<<"\t"<<"Total\n";
- cout<<"---------------------------------------------------------------\n";
- for (i = 0; i < index; i++) {
- cout<<a.num[i]<<"\t\t"<<a.name[i]<<"\t  "<<a.count[i]<<"\t\t"<<a.price[i]<<"\t"<<a.total[i]<<"\n";
-
- }
- cout <<"---------------------------------------------------------------\n";
- cout<<"Total Prices :" <<a.total_price(a)<<"\n";
- cout<<"Total Quantity :" <<a.total_num(a)<<"\n";
-
- }
-
-
-
- double Factor ::total_price(Factor a){    // Total Price Method
-	double sum;
-	int i;
-	for (i = 0; i < index; i++) {
-		sum = sum + a.price[i]*a.count[i] ;
-	}
-	return sum;
-	}
-	double Factor ::total_num(Factor a){
-	int i;
-	double sum;
-	for (i = 0; i < index; i++) {
-			sum = sum + a.count[i];
-
-	}
-	return sum;
-
-	}
- void Factor::save(Factor a , int o ){   // Save Method
-	file.open("Factor.txt");
-	file<<"\t\tPlease Enter Factor Information\t"<<" -- Factor Number : "<<o<<endl;
-	file<<"#"<<"\t"<<"\t"<<"Name"<<"\t"<<"Quantitiy"<<"\t"<<"Price"<<"\t"<<"Total\n\n";
-	for (int i = 0; i < index; i++) {
- file<<a.num[i]<<"\t\t"<<a.name[i]<<"\t  "<<a.count[i]<<"\t\t"<<a.price[i]<<"\t"<<a.total[i]<<"\n";
- }
- file.close();
- }
 
 
 
@@ -143,11 +64,87 @@ int factor_index=1;
 	int d ;
 	double e;
 	int ask;
+	Factor temp[5];
+	store total;
+	int k;
 
 	// Real time Get From PC CPU
 
+	main_loop :
+	cout<<"Enter Store [1] Or Create a Factor [2]";
+	cin>>ask;
+	if (ask==1) {
+	  goto gotostore;
+	}
+	else if (ask==2) {
+			 goto newfactor;
 
-	 Factor temp[5];
+		 }
+		 else{
+			 cout<<"Enter Invalid Number";
+			 goto main_loop;
+		 }
+	gotostore:
+
+	ask_s:
+	cout<<"Do you want to add store[1] or check store[2] or change store[3]?"<<endl;
+	cin>>k;
+	if(k==1)goto add;
+	if (k==2)goto check;
+	if (k==3) goto change;
+	else{
+	cout<<"Invalid number, try again"<<endl;
+	goto gotostore;
+	}
+
+	add:
+	cout<<"enter ID/ name / Quantity"<<endl;
+		cin>>id;
+		cin>>nam;
+		cin>>number;
+	total.add_store(id,number,nam);
+	goto gotostore;
+
+	check:
+	cout<<"plz enter good's ID";
+	cin>>id;
+	for (int j=0, flag=0; j < 100; j++) {
+			if(total.ID[j]==id)
+				{
+					flag=1;
+					total.show_store(j,total);
+
+				}
+			if(flag==0) {
+			cout<<"invalid ID,nothing was found"<<endl;
+			goto gotostore;
+			}
+	}
+	goto gotostore;
+
+	change:
+	cout<<"plz enter good's ID"<<endl;
+	cin>>id;
+	for (int j=0, flag=0; j < 100; j++) {
+			if(total.ID[j]==id)
+			{
+					int t;
+					cout<<"plz enter your good's number:"<<endl;
+					cin>>t;
+					total.num[j]=t;
+					//total.update(total,j,t);
+					//total.show_store(j,total);
+					flag=1;
+			}
+			if(flag==0) {
+			cout<<"invalid ID,nothing was found"<<endl;
+			goto gotostore;
+			}
+			}
+
+
+
+
 	 newfactor:
 	 time_t t=time(0);
 	struct tm * now = localtime( & t );
@@ -177,7 +174,8 @@ int factor_index=1;
 	cin>>ask;
 	if (index>255) {
 		index=0;
-		cout<<"End Of This Factor - New Factor[1] , Print And Save  Factor[2]";
+		menu_ask:
+		cout<<"End Of This Factor - New Factor[1] , Print And Save  Factor[2] , Main Menu [3]";
 		cin>>ask;
 		if (ask==1) {
 
@@ -203,6 +201,13 @@ int factor_index=1;
 						 goto ask1_label;
 					 }
 			 }
+			 else if (ask==3) {
+					  goto main_loop;
+				  }
+				  else{
+					  cout<<"Enter Correct Number" ;
+					  goto menu_ask;
+                  }
 	}
 	if (ask==1) {
 		goto label;
@@ -211,7 +216,7 @@ int factor_index=1;
 			temp[factor_index].print(temp[factor_index]) ;
 			temp[factor_index].save(temp[factor_index] , factor_index);
 			ask2_label:
-				cout<<"Exit [1] , New Factor[2]";
+				cout<<"Exit [1] , New Factor[2] , Main_Menu[3]";
 				cin>>ask;
 				if (ask==1) {
 							  goto exit_label;
@@ -225,6 +230,9 @@ int factor_index=1;
 						 goto ask2_label;
 					 }
 			 }
+			 else if(ask==3){
+			   goto main_loop;
+             }
 			 else{
 				 cout<<"Invalid Input"<<endl;
 				 goto label;
@@ -235,6 +243,8 @@ int factor_index=1;
 
 
  exit_label :
+
+ exit:
 
 
 
